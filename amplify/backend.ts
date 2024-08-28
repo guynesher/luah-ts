3adcff604b67686a195abc74f8ef1e2c3b9d6981
guynesher <guynesher2000@gmail.com>
2024-08-28 11:31:36 +0300

    storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,16 +1,18 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { storage } from './storage/resource';
 
 const backend = defineBackend({
   auth,
   data,
+  storage,
 });
 
 
-backend.addOutput({
-  storage: {
-    aws_region: "eu-central-1",
-    bucket_name: "lu-ah-s373636-master"
-  },
-});
+// backend.addOutput({
+//   storage: {
+//     aws_region: "eu-central-1",
+//     bucket_name: "lu-ah-s373636-master"
+//   },
+// });
