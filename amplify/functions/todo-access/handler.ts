f99517a7a7aeccd5bf8bf3c59d5ad021430891c3
guynesher <guynesher2000@gmail.com>
2024-09-03 15:34:54 +0300

    getTodoByAPI

--- amplify/functions/todo-access/handler.ts
+++ amplify/functions/todo-access/handler.ts
@@ -1,7 +1,37 @@
 import type { APIGatewayProxyHandler } from "aws-lambda";
+import { env } from '$amplify/env/todo-access';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   console.log("event", event);
+  const query = `
+    query MyQuery {
+      listTodos {
+        items {
+          content
+          createdAt
+          id
+          updatedAt
+        }
+      }
+    }
+`;  
+
+  const graphqlQuery = {
+    "operationName": "MyQuery",
+    "query": query,
+    "variables": {}
+  };
+
+  const options = {
+    "method": "POST",
+    "headers": {
+      "x-api-key": env.APIKEY,
+      "Content-Type": "application/json"
+    },
+    "body": JSON.stringify(graphqlQuery)
+  };
+  const response = await fetch(env.APIURL, options);
+  const res = await response.json();
   return {
     statusCode: 200,
     // Modify the CORS settings below to match your specific requirements
@@ -9,6 +39,6 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       "Access-Control-Allow-Origin": "*", // Restrict this to domains you trust
       "Access-Control-Allow-Headers": "*", // Specify only the headers you need to allow
     },
-    body: JSON.stringify("Hello from myFunction!"),
+    body: JSON.stringify({res:res,name:env.NAME}),
   };
 };
--- amplify/functions/todo-access/resource.ts
+++ amplify/functions/todo-access/resource.ts
@@ -1,6 +1,11 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { defineFunction, secret } from '@aws-amplify/backend';
 
 export const todoAccess = defineFunction({
   name: 'todo-access',
-  entry: './handler.ts'
+  entry: './handler.ts',
+  environment: {
+    NAME: "Luah",
+    APIKEY: secret('APKEY'),
+    APIURL: secret('APURL')
+  }
 });
